97e234d10ba451f8ee70c445dd274cf0fb35c329
Benson1198 <bensonpx1198@gmail.com>
2021-11-17 12:02:25 +0530

    Added decimal feature

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput:TextView? = null
+    var lastNumeric:Boolean = false
+    var lastDot:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+    }
+
+
+    fun onDigit(view:View){
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+    }
+
+    fun onClear(view:View){
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view:View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+
     }
 }
